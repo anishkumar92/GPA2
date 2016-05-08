f8fb2039d10dab95b57b97c2e8f60eb20251b2d4
anishkumar92 <anishkumar92@gmail.com>
2016-05-09 04:35:35 +0530

    bootstap integrated

---
+++ application/views/guest/index.php
@@ -0,0 +1,5 @@
+<center><h3>
+<ul class="list-unstyled">
+<?php echo '<li><a href="'.site_url("guest/calculator/gpa").'">Calculate GPA</a></li><li><a href="'.site_url("guest/calculator/cgpa").'">Calculate CGPA</a></li>';?>
+</ul>
+</h3></center>
---
+++ application/views/guest/table.php
@@ -0,0 +1,15 @@
+<html>
+<head>
+    <title><?php echo $type; ?></title>
+</head>
+<body>
+    <center>
+        <h2><?php echo $title; ?></h2><br>
+        <form action="<?php echo site_url('guest/result');?>" method="post">
+        <?php echo $sub_table; ?><br>
+        <?php echo '<input type="hidden" name ="type" value="'.$type.'">'; ?>
+        <?php echo '<input type="submit" value="Get my '.$type.'"></form>'; ?>
+    </br>  </br>  </br>  </br>  </br>
+    </center>
+</body>
+</html>
